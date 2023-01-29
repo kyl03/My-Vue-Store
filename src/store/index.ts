196384033412c6228f81af9c37e31f8d6d855278
Karen Yep <karenyeplee@gmail.com>
2023-01-28 20:14:42 -0600

    store index created

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,33 @@
-import { createStore } from "vuex";
+
+import { User } from '@/models/user';
+import { createStore } from 'vuex'
+import cartModule from './cart';
+import productsModule from './products';
+import usersModule from './users';
+
+export interface IState {
+  authUser: User | null;
+  greeting: string;
+}
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state: {
+    authUser: null,
+    greeting: 'Bienvenida',
+    token: '',
+  },
+  getters: {
+    saludo(state) {
+      return state.greeting;
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    cart: cartModule,
+    users: usersModule,
+    products: productsModule
+  }
+})
