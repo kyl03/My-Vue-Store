e69a19704929e572dd15239877fd2373249da3a5
Karen Yep <karenyeplee@gmail.com>
2023-01-28 20:25:28 -0600

    App.vue set

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <NavBar
+title="My Store"
+/>
+<div v-if="userIsLoggedIn">
+<NavBarLinks/>
+</div>
+<router-view />
+<CartList v-show="isCartOpen" />
 </template>
 
+<script lang="ts">
+import { defineComponent, onBeforeMount, onMounted } from "vue";
+import NavBar from "@/components/NavBar.vue";
+import NavBarLinks from "@/components/NavBarLinks.vue";
+import CartList from "./components/CartList.vue";
+import useUsers from "./composables/useUsers";
+import { RouterView } from "vue-router";
+// import DetailView from "./views/DetailView.vue"
+export default defineComponent({
+name: "AppComponent",
+components: {
+NavBar,
+CartList,
+NavBarLinks
+// DetailView
+},
+setup() {
+
+const{userIsLoggedIn, checkIfUserIsLoggedIn} = useUsers();
+checkIfUserIsLoggedIn();
+return {
+  userIsLoggedIn
+};
+},
+});
+</script>
+
+
 <style>
+* {
+padding: 0;
+margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
+/* 
 nav a.router-link-exact-active {
-  color: #42b983;
-}
+color: #42b983;
+} */
 </style>
