f3106a37800f5da8ea4bc12b166d323706fdb076
Karen Yep <karenyeplee@gmail.com>
2023-01-28 20:11:45 -0600

    products mutations created

---
+++ src/store/products/mutations.ts
@@ -0,0 +1,22 @@
+import { Product } from "@/models/product";
+import { MutationTree } from "vuex";
+import { IState } from "..";
+import { IProductsState } from "./state";
+
+const mutations: MutationTree<IProductsState> = {
+    setProducts(state: IProductsState, products: Product[]){
+        state.products = products;
+    },
+    setIsloading(state: IProductsState, value: boolean){
+        state.isLoading = value;
+    },
+    setSelectedProduct(state, product: Product){
+        state.selectedProduct = product;
+    },
+    setFilteredProducts(state, filteredProducts: Product[]){
+        state.products = filteredProducts;
+    }
+
+};
+
+export default mutations;
