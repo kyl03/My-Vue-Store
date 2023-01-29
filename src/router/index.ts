f16f2831a43cae2784895cca4e7efb5148ef6e0f
Karen Yep <karenyeplee@gmail.com>
2023-01-28 19:55:03 -0600

    Role-guard created

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-
+    beforeEnter: (loggedInGuard),
     component: HomeView
   },
   {
@@ -20,7 +20,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/profile',
     name: 'profile',
- 
+    beforeEnter: (loggedInGuard),
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
---
+++ src/router/role-guard.ts
@@ -0,0 +1,15 @@
+import { RouteLocation } from "vue-router";
+
+const haveRoleGuard = (to:RouteLocation,from:RouteLocation,next:Function)=>{
+    console.log(to, from, next);
+    const userRole = localStorage.getItem('userRole');
+    if(userRole ==='Admin'){
+        next();
+    }else{
+        alert('No eres admin! no puedes pasar!')
+        next({name:'login'});
+    }
+  
+}
+
+export default haveRoleGuard;
