6c886cf857cf0e4289e42972096b97b5b71c2376
Karen Yep <karenyeplee@gmail.com>
2023-01-28 19:59:38 -0600

    product model created

---
+++ src/models/product.ts
@@ -0,0 +1,18 @@
+export interface Product {
+    id:          number;
+    title:       string;
+    price:       number;
+    description: string;
+    images:      string[];
+    creationAt:  string;
+    updatedAt:   string;
+    category:    Category;
+}
+
+export interface Category {
+    id:         number;
+    name:       string;
+    image:      string;
+    creationAt: string;
+    updatedAt:  string;
+}
