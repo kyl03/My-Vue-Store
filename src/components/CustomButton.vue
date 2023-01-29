1cd8ac9b3d6108fab313d9e93eac8e2b1dbef062
Karen Yep <karenyeplee@gmail.com>
2023-01-28 20:03:44 -0600

    CustonButton component created

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,14 @@
+<template>
+   
+    <button type="button" class="btn btn-outline-dark">
+     <slot>
+     </slot>
+     </button>
+ </template>
+ 
+ <script lang="ts">
+ import { defineComponent } from 'vue';
+ export default defineComponent({
+     name: "CustomButton",
+ });
+ </script>
