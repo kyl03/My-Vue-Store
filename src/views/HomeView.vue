b664c944def8463a491d4c1f2495a64af07fd717
Karen Yep <karenyeplee@gmail.com>
2023-01-28 20:27:31 -0600

    HomelView set

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,67 @@
 <template>
+
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+      <div v-if="isLoading">Cargando...</div>
+      <div v-else>
+          <div class="search-bar">
+              <input v-model="inSearchProduct" type="text" placeholder="Search..">
+              <CustomButton @click="search(inSearchProduct)">Search</CustomButton>
+          </div>
+          <div class="user-list" v-if="products.length > 0">
+              <ProductItem v-for="product in products" :key="product.id" :product="product"
+                   @goDetail="goDetail" />
+          </div>
+          <div v-else>
+              <p>0 products found</p>
+          </div>
+      </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import { defineComponent, ref } from "vue";
+import ProductItem from "@/components/ProductItem.vue";
+import { useRouter } from "vue-router";
+import useProducts from "@/composables/useProducts";
+import { Product } from "@/models/product";
+import products from "@/store/products";
+import CustomButton from "@/components/CustomButton.vue";
+import useUsers from "@/composables/useUsers";
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+      ProductItem,
+      CustomButton
+  },
+  setup() {
+      let inSearchProduct = ''
+      let { products, isLoading, fetchProducts, fetchProductsByFilter } = useProducts();
+      const{userIsLoggedIn, checkIfUserIsLoggedIn} = useUsers();
+      const router = useRouter();
+      checkIfUserIsLoggedIn();
+      fetchProducts();
+
+      return {
+          products,
+          isLoading,
+          userIsLoggedIn,
+          inSearchProduct,
+          search: (inSearchProduct: string) => inSearchProduct.length > 2 ? fetchProductsByFilter(inSearchProduct) : null,
+          goDetail: (product: Product) =>
+              router.push({ name: "detail", params: { id: product.id } }),
+      };
   },
 });
 </script>
+
+<style scoped>
+.user-list {
+  display: flex;
+  flex-flow: row wrap;
+  width: 100%;
+  gap: 1rem 1rem;
+}
+.search-bar {
+  width: 100%;
+}
+</style>
