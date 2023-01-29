7f1abb5e93d445ee72ae86fec31f7512c792dc2a
Karen Yep <karenyeplee@gmail.com>
2023-01-28 20:12:46 -0600

    products index created

---
+++ src/store/products/index.ts
@@ -0,0 +1,16 @@
+import { Module } from "vuex";
+import { IState } from "..";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
+import state, { IProductsState } from "./state";
+
+const productsModule: Module<IProductsState, IState>={
+    namespaced: true, //para que Vue lo separe como modulo
+    state,
+    getters,
+    mutations,
+    actions
+};
+
+export default productsModule;
